b7ae6ae70777279cba3405d151cbb8cc61215b7d
bulbul13072 <mr.bulbul.rbl@gmail.com>
2021-08-27 13:59:35 +0600

    mongo not connected

--- Index.js
+++ Index.js
@@ -1,11 +1,19 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+require('dotenv').config();
 
-
-
-
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(8800, () => {
-    console.log ("Kaam Choler");
+  console.log("Backend server is running!");
 });
